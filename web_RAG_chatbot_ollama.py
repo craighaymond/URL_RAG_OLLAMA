f3b2f29b84ee2f7dee01d6dcb7da4c9ff5b72b46
Craig Haymond <94215978+craighaymond@users.noreply.github.com>
2024-04-20 01:29:17 -0700

    Add files via upload

---
+++ web_RAG_chatbot_ollama.py
@@ -0,0 +1,61 @@
+# starting point source: https://www.linkedin.com/pulse/how-build-rag-chatbot-using-ollama-serve-llms-locally-sri-laxmi-beapc/
+# with required modifications by chaymond
+import streamlit as st
+
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import Chroma
+from langchain_community import embeddings
+from langchain_community.llms import Ollama
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.embeddings.ollama import OllamaEmbeddings
+
+# URL processing
+def process_input(urls, question):
+    model_local = Ollama(model="llama3")
+
+    # Convert string of URLs to list
+    urls_list = urls.split("\n")
+    docs = [WebBaseLoader(url).load() for url in urls_list]
+    docs_list = [item for sublist in docs for item in sublist]
+
+    # split the text into chunks.  "size" units are in # of characters.
+    text_splitter = CharacterTextSplitter.from_tiktoken_encoder(chunk_size=7500, chunk_overlap=100)
+    doc_splits = text_splitter.split_documents(docs_list)
+
+    # convert text chunks into embeddings and store in vector database
+    vectorstore = Chroma.from_documents(
+        documents=doc_splits,
+        collection_name="rag-chroma",
+        embedding=embeddings.ollama.OllamaEmbeddings(model='nomic-embed-text'),
+    )
+    retriever = vectorstore.as_retriever(search_kwargs={"k": 1})
+
+    # perform the RAG
+    after_rag_template = """Answer the question based only on the following context:
+    {context}
+    Question: {question}
+    """
+    after_rag_prompt = ChatPromptTemplate.from_template(after_rag_template)
+    after_rag_chain = (
+            {"context": retriever, "question": RunnablePassthrough()}
+            | after_rag_prompt
+            | model_local
+            | StrOutputParser()
+    )
+    return after_rag_chain.invoke(question)
+
+st.title("Document Query with Ollama")
+st.write("Enter URLs (one per line) and a question to query the documents.")
+
+# Input fields
+urls = st.text_area("Enter URLs separated by new lines", height=150)
+question = st.text_input("Question")
+
+# Button to process input
+if st.button('Query Documents'):
+    with st.spinner('Processing...'):
+        answer = process_input(urls, question)
+        st.text_area("Answer", value=answer, height=300, disabled=True)
